8346965550b45d3a72dcc9acb51ac7bc5d764078
khannac456@gmail.com <khannac456@gmail.com>
2025-10-14 23:12:32 +0530

    final123

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,16 +1,9 @@
 import os
 
-# Bind to PORT environment variable
 bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
-
-# Worker configuration
 workers = 4
 worker_class = 'sync'
-
-# Timeout configuration
 timeout = 120
-
-# Logging configuration
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
